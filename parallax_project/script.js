a2bdf951dd82934109337f189d9394b5983d6d25
Harshitha G <harshithaganesh08@gmail.com>
2025-09-10 21:53:15 +0530

    parallax

--- parallax_project/script.js
+++ parallax_project/script.js
@@ -13,4 +13,5 @@ window.addEventListener('scroll', () => {
     hill5.style.left = value * 1.5 + 'px';
     hill4.style.left = value * -1.5 + 'px';
     hill1.style.top = value * 1.5 + 'px';
+    hill1.style.left = value * -1.5 + 'px';
 });
