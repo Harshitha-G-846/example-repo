a5eddc9a1c8aa34c855921c881870b069cfb4d00
Harshitha G <harshithaganesh08@gmail.com>
2025-09-11 16:28:21 +0530

    Update script.js

--- parallax_project/script.js
+++ parallax_project/script.js
@@ -6,8 +6,9 @@ let hill5 = document.getElementById('hill5');
 let tree = document.getElementById('tree');
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
+    
 
-    text.style.marginBottom = value * 2.5 + 'px';
+    text.style.marginTop = value * 2.5 + 'px';
     leaf.style.top = value * -1.5 + 'px';
     leaf.style.left = value * 1.5 + 'px';
     hill5.style.left = value * 1.5 + 'px';
