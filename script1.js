9edbb3779f59966ed1bb3d59eaf0a595c0956d71
Harshitha G <harshithaganesh08@gmail.com>
2025-09-17 23:28:45 +0530

    update

---
+++ script1.js
@@ -0,0 +1,18 @@
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
+let hill1 = document.getElementById('hill1');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+    let limit = Math.min(window.scrollY, 10);
+
+    text.style.marginTop = limit * 2.5 + 'px';
+    leaf.style.top = limit * -1.5 + 'px';
+    leaf.style.left = limit * 1.5 + 'px';
+    hill5.style.left = limit * 1.5 + 'px';
+    hill4.style.left = limit * -1.5 + 'px';
+    hill1.style.top = limit * 1.5 + 'px';
+
+});
