70b2f92eef81b377e89079c39d06349dc9c0ec34
Harshitha G <harshithaganesh08@gmail.com>
2025-09-18 21:31:13 +0530

    update

---
+++ java_project/demo1.java
@@ -0,0 +1,11 @@
+class sample {
+    int a = 20;
+    int b = 30;
+}
+class demo1{
+    public static void main(String[] args) {
+        sample obj = new sample();
+        System.out.println(obj.a);
+        System.out.println(obj.b);
+    }
+}
