3f61002dcdec5aa99071d5bda3678d211ea3b41a
Harshitha G <harshithaganesh08@gmail.com>
2025-09-19 19:42:55 +0530

    sum

--- java_project/demo1.java
+++ java_project/demo1.java
@@ -1,11 +1,22 @@
-class sample {
-    int a = 20;
-    int b = 30;
-}
-class demo1{
+public class demo1
+{
     public static void main(String[] args) {
-        sample obj = new sample();
-        System.out.println(obj.a);
-        System.out.println(obj.b);
+        double[] myList = {1.9, 2.9, 3.4, 3.5};
+        // Print all the array elements
+        for (int i = 0; i < myList.length; i++) {
+            System.out.println(myList[i] + " ");
+        }
+    // Summing all elements
+        double total = 0;
+        for (int i = 0; i < myList.length; i++) {
+            total += myList[i];
+        }
+        System.out.println("Total is " + total);
+    // Finding the largest element
+        double max = myList[0];
+        for (int i = 1; i < myList.length; i++) {
+            if (myList[i] > max) max = myList[i];
+        }
+        System.out.println("Max is " + max);
     }
 }
